f92a143f044bd48d6f91b27d5390f3420c6f07f9
dom <davide.domini2@unibo.it>
2023-05-03 14:48:02 +0200

    chore: add tuprolog dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,17 @@ repositories {
 }
 
 dependencies {
-    implementation("it.unibo.tuprolog:solve:0.31.5")
+    /*implementation("it.unibo.tuprolog:solve:0.31.5")
     implementation("it.unibo.tuprolog:solve-classic:0.31.5")
     implementation("it.unibo.tuprolog:core:0.31.5")
     implementation("it.unibo.tuprolog:theory:0.31.5")
     implementation("it.unibo.tuprolog:unify:0.31.5")
+    implementation("it.unibo.tuprolog:parser-core:0.31.5")
+    implementation("it.unibo.tuprolog:parser-theory:0.31.5")
+    implementation("it.unibo.tuprolog:parser-theory-jvm:0.31.5")*/
+    //implementation("it.unibo.tuprolog:parser-theory:0.31.3")
+    //implementation("it.unibo.tuprolog:parser-jvm:0.31.5")
+    implementation("it.unibo.tuprolog:full:0.31.3")
     testImplementation(kotlin("test"))
 }
 
---
+++ src/main/kotlin/it/unibo/Main.kt
@@ -0,0 +1,23 @@
+import it.unibo.tuprolog.core.Struct
+import it.unibo.tuprolog.core.Var
+import it.unibo.tuprolog.solve.classic.ClassicSolverFactory
+import it.unibo.tuprolog.theory.parsing.ClausesParser
+
+fun main(args: Array<String>) {
+    println("Start")
+
+    val t = with(ClausesParser.withStandardOperators()){
+        parseTheory("f(davide).\nf(simone).")
+    }
+
+    println("clauses")
+    t.clauses.forEach{ println(it) }
+    println("directives")
+    t.directives.forEach{ println(it) }
+
+    val s = ClassicSolverFactory.solverOf(staticKb = t)
+    val X = Var.of("X")
+    s.solve(Struct.of("f", X)).forEach { println(it.substitution[X]) }
+
+    println("End")
+}

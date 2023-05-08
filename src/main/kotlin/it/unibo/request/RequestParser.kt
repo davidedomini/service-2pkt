76fb7263ac85049a0ad3f62f839a3b27533c77ff
dom <davide.domini2@unibo.it>
2023-05-08 15:34:50 +0200

    feat: implement request parsing from json

--- src/main/kotlin/it/unibo/request/RequestParser.kt
+++ src/main/kotlin/it/unibo/request/RequestParser.kt
@@ -1,4 +1,11 @@
 package it.unibo.request
 
+import com.google.gson.Gson
+
 class RequestParser {
+
+    fun parseRequest(request: String): ComputationRequest {
+        return Gson().fromJson(request, ComputationRequest::class.java)
+    }
+
 }

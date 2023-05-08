044a11c1c8882eac9774f3d4f62ab0869ec169c0
dom <davide.domini2@unibo.it>
2023-05-08 15:21:01 +0200

    feat: update computation request

--- src/main/kotlin/it/unibo/request/ComputationRequest.kt
+++ src/main/kotlin/it/unibo/request/ComputationRequest.kt
@@ -1,7 +1,12 @@
 package it.unibo.request
 
+import it.unibo.model.ComputationType
+
 class ComputationRequest(
     val id: String,
     val theory: String,
     val goal: String,
+    val timeout: Long = -1,
+    val maxSol: Int = -1,
+    val type: ComputationType
 )
